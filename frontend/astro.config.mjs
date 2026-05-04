dc21498c75f87d1274b017be7f3065f81808f56e
gpavrian <skopetos@Utility.>
2026-05-04 17:01:17 +0300

    bug

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
   site: 'https://incopter.gr',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
   output: 'static',
   devToolbar: { enabled: false },
 });
